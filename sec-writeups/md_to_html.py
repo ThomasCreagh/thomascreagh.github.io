ed403da73cb6ee476b929ea28ea86a5e65576721
Thomas Keatinge Creagh <tkeating@tcd.ie>
2025-07-26 13:00:05 +0100

    breif sec pages link page

--- sec-writeups/md_to_html.py
+++ sec-writeups/md_to_html.py
@@ -4,6 +4,17 @@ import markdown
 print(f"the current working dir is: {os.getcwd()}")
 
 files = os.listdir("md")
+
+page_links = [
+    f'<li><a href="html/{i.replace("md", "html")}">{i.split(".")[0]}</a></li>' for i in files]
+html_content = '<head><link rel="stylesheet" href="../css/styles.css"></head>\n' + \
+    f'<body><h1>Security write ups</h1><ul>{str(page_links)[1:-1].replace(",","")}</ul></body>'
+with open("index.html", "w", encoding="utf-8") as html_file:
+    html_file.write(html_content)
+
+print("wrote index.html succefully")
+
+
 for file in files:
     with open(f"md/{file}", "r", encoding="utf-8") as md_file:
         md_content = md_file.read()
